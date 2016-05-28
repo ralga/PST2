ec667c2833da89dde1f8ed6f13381214d3719e82
Marc-Antoine de Drouas <marc-antoine.dedrouas@epita.fr>
2016-05-28 18:08:33 +0200

    correction bug dans la capacité reset

--- src/PST2/Capacity/Reset.java
+++ src/PST2/Capacity/Reset.java
@@ -13,13 +13,13 @@ import java.util.ArrayList;
  *
  * @author mad
  */
-public class Reset extends Capacity{
-    
+public class Reset extends Capacity {
+
     private static final int COOL = 10;                                         //Cooldown propre à chaque capacité
     private final static int CAST = 0;                                          //Temps de Cast propre à chaque capacité
 
     public Reset(Piece piece, int id) {
-        super(piece, COOL, CAST, id, true,"Recharge des capacités");
+        super(piece, COOL, CAST, id, true, "Recharge des capacités");
         init();
     }
 
@@ -27,12 +27,14 @@ public class Reset extends Capacity{
     public void setfire() {
         ArrayList<Capacity> pass = getPassive(piece.getTeam());
         ArrayList<Capacity> act = getActive();
-        
-        for(Capacity p : pass)
-            p.setCool(0);
-        for(Capacity p : act)
-            if(p.piece.getTeam()==piece.getTeam())
+
+        for (Capacity p : pass)
+            if (p.getCurrentCool() != -1)
                 p.setCool(0);
+        for (Capacity p : act)
+            if (p.piece.getTeam() == piece.getTeam())
+                if (p.getCurrentCool() != -1)
+                    p.setCool(0);
         StratEdge.getSE().getGame().setSelection(null);
         StratEdge.getSE().getGame().setTurn();
     }
@@ -41,5 +43,5 @@ public class Reset extends Capacity{
     protected void reset() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
